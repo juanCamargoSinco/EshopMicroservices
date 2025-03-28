56fe73f209a50f1509033be682a7608d07ece4b0
Juan Manuel Camargo Segura <juan.camargo@sinco.co>
2025-03-28 15:52:13 -0500

    Catalog API

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+
+    internal class CreateProductComandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Logica de negocio para crear un producto
+            // Crear un Product a partir del command object
+            // Guardar en la DB
+            // return CreateProductResult result
+
+            // Crear un Product a partir del command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // Guardar en la DB
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // return CreateProductResult result
+            return new CreateProductResult(product.Id);
+            
+        }
+    }
+}
