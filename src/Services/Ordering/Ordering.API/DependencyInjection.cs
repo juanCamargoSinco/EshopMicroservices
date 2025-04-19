5158c174f5932cb9fbe01025f68fbf5b84497f0f
Juan Manuel Camargo Segura <juan.camargo@sinco.com.co>
2025-04-19 13:08:29 -0500

    seccion 13

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Carter;
+
+namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    //Antes del build la aplicacion
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //services.AddCarter();
+
+        return services;
+    }
+    //Despues del buil de la aplicacion 
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.MapCarter();
+
+        return app;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static class DependencyInjection
+{
+    //IConfiguration para acceder a las cadenas de conexion
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //Se obtiene la cadena de conexion de Ordering.API
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        //{
+        //    options.UseSqlServer(connectionString);
+        //});
+
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+}
