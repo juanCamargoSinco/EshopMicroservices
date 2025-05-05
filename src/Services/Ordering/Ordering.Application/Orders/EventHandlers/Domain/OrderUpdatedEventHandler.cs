f0c49ee6bb0e503e3540655c59be83c013984f7f
Juan Manuel Camargo Segura <juan.camargo@sinco.com.co>
2025-05-05 08:14:32 -0500

    seccion 16

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+public class OrderUpdatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderUpdatedEvent>
+{
+    public Task Handle(OrderUpdatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+
+        var orders = await dbContext.Orders
+                //Incluye los order items amarrados a la orden
+                .Include(o => o.OrderItems)
+                //Cuando solo se leen entidades y no se van a modificar en el mismo contexto, usar asnotracking
+                .AsNoTracking()
+                .Where(o => o.OrderName.Value.Contains(query.Name))
+                .OrderBy(o => o.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
