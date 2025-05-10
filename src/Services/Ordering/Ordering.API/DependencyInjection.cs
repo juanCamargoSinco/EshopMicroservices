2a39492a50f64e023c226728e982b9e4113f2473
Juan Manuel Camargo Segura <juan.camargo@sinco.com.co>
2025-05-09 23:33:34 -0500

    seccion 17

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,20 +1,39 @@
-﻿using Carter;
+﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 
 public static class DependencyInjection
 {
     //Antes del build la aplicacion
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
-        //services.AddCarter();
+        services.AddCarter();
+
+        //Adicion de manejo de excepciones personalizadas
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
-    //Despues del buil de la aplicacion 
+    //Despues del build de la aplicacion 
     public static WebApplication UseApiServices(this WebApplication app)
     {
-        //app.MapCarter();
+        app.MapCarter();
+
+        //Configuracion de middleware para el manejo de excepciones
+        //Al pasar una lambda vacía ({ }), no se configura ningún comportamiento de manejo de errores.
+        app.UseExceptionHandler(options => { });
+
+        //Configuracion para hacer el health check legible
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return app;
     }
---
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -0,0 +1,27 @@
+﻿    using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.API.Endpoints;
+
+public record UpdateOrderRequest(OrderDto Order);
+public record UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/updateorder", async (UpdateOrderRequest request, ISender sender) =>
+        {
+
+            var command = request.Adapt<UpdateOrderCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("Update order")
+        .Produces<bool>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
